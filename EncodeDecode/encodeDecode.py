0eb183c6dc19f3755e2939d6735b1bb6a22fbe2d
loopsidoop <lawmacj16@gmail.com>
2023-08-20 02:06:56 +0800

    Update encodeDecode.py

--- EncodeDecode/encodeDecode.py
+++ EncodeDecode/encodeDecode.py
@@ -2,13 +2,14 @@ from tkinter import *
 import base64
 
 window = Tk()
-window.geometry("840x600")
 
 label = Label(window, text ='ENCODE DECODE', font = ("Arial",12,"bold"))
-label.pack()
+label.grid(row=0, column=0,columnspan=2)
 
 # Variables to be used
-message, private_key, mode, result, = "","","",""
+message = StringVar()
+private_key = StringVar()
+result = StringVar()
 
 def Encode(key,message):
     enc=[]
@@ -26,5 +27,25 @@ def Decode(key,message):
         key_c = key[i % len(key)]
         dec.append(chr((256 + ord(message[i])- ord(key_c)) % 256))
     return "".join(dec)
+
+def activate_encode():
+    result.set(Encode(private_key.get(), message.get()))
+def activate_decode():
+    result.set(Decode(private_key.get(), message.get()))
+        
+message_label = Label(window, text="Message", font=("Arial",14)).grid(row=1,column=0)  
+message_entry = Entry(window, font=("Arial", 10), textvariable = message).grid(row=1,column=1)
+
+key_label = Label(window, text="Private Key", font=("Arial",14))
+key_label.grid(row=2,column=0)   
+
+key_entry = Entry(window, font=("Arial", 10), textvariable = private_key)
+key_entry.grid(row=2,column=1)
+
+mode_encode = Button(window, text="Encode", command = activate_encode, font=("Arial",10)).grid(row=2,column=2)
+mode_decode = Button(window, text="Decode", command = activate_decode, font=("Arial",10)).grid(row=2,column=3)
+
+result_label = Label(window, text="Result", font=("Arial", 14)).grid(row=3,column=0)
+final_result = Entry(window, textvariable=result).grid(row=3,column=1)
 
 window.mainloop()
