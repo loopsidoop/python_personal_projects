7f9711275a96892a931e87f256e7837ebc80a01a
loopsidoop <lawmacj16@gmail.com>
2023-08-20 15:52:16 +0800

    Update kryptogui.py

--- KryptoGame/kryptogui.py
+++ KryptoGame/kryptogui.py
@@ -2,20 +2,17 @@ import random
 from tkinter import *
 
 window = Tk()
-window.geometry('400x200')
+window.geometry('400x300')
 window.resizable(0,0)
 window.title("Krypto Generator")
 
-def krypto_generate():
-    krypto_possibilities = [1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 8, 9, 9, 9, 10, 10, 10,
-                            11, 11, 12, 12, 13, 13, 14, 14, 15, 15, 16, 16, 17, 17, 18, 19, 20, 21, 22, 23, 24, 25]
-    krypto_list = []
-    while len(krypto_list) < 6:
-        krypto = random.choice(krypto_possibilities) # finds a random number in the list
-        krypto_possibilities.remove(krypto) # removes the number from the list
-        krypto_list.append(str(krypto))
-    return krypto_list
-
 title = Label(window, text="Krypto Generator", font=("Arial", 14, 'bold')).pack()
+target_num = Label(window, text="1", font=("Calibri",25,'bold'),width=20,height=1, bg='ghost white',relief=SOLID).place(x=25,y=30)
+
+num1 = Label(window, text="2", font=("Calibri",25,'bold'),width=3,height=1, bg='ghost white',relief=SOLID).place(x=25,y=80)
+num2 = Label(window, text="2", font=("Calibri",25,'bold'),width=3,height=1, bg='ghost white',relief=SOLID).place(x=97,y=80)
+num3 = Label(window, text="2", font=("Calibri",25,'bold'),width=3,height=1, bg='ghost white',relief=SOLID).place(x=169,y=80)
+num4 = Label(window, text="2", font=("Calibri",25,'bold'),width=3,height=1, bg='ghost white',relief=SOLID).place(x=241,y=80)
+num5 = Label(window, text="2", font=("Calibri",25,'bold'),width=3,height=1, bg='ghost white',relief=SOLID).place(x=313,y=80)
 
 window.mainloop()
