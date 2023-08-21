f9b67d70770a79c77d0202493770d0cc72515890
loopsidoop <lawmacj16@gmail.com>
2023-08-21 15:33:27 +0800

    xd

---
+++ PrimeNumGenerator/primeNumGenGUI.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+window = Tk()
+window.geometry('500x350')
+window.resizable(0,0)
+
+Label(window, text="Prime Numbers Generator", font=("Arial", 16, 'bold')).pack()
+
+window.mainloop()
