deda7eb82b01d960fe12541d5076ad5c15a2f653
loopsidoop <lawmacj16@gmail.com>
2023-08-24 21:01:34 +0800

    Update passGenGui.py

--- PasswordGenerator/passGenGui.py
+++ PasswordGenerator/passGenGui.py
@@ -29,10 +29,10 @@ def activate_password_gen():
 
 Label(window, text="Password Generator", font=("Arial", 14, 'bold')).pack()
 
-message_label = Label(window, text="Password Length", font=("Arial",11,'bold')).place(x=60,y=45)
-message_entry = Entry(window, font=("Arial", 10), textvariable = pass_length,bg = 'ghost white').place(x=220,y=45)
+plength_label = Label(window, text="Password Length", font=("Arial",11,'bold')).place(x=60,y=45)
+plength_entry = Entry(window, font=("Arial", 10), textvariable = pass_length,bg = 'ghost white').place(x=220,y=45)
 
 generate = Button(window, text="GENERATE PASSWORD",font=("Arial",12,'bold'), command = activate_password_gen).place(x=150,y=80)
-final_result = Entry(window, textvariable=userPassword, width=30, bg ='ghost white').place(x=158,y=120)
+result = Entry(window, textvariable=userPassword, width=30, bg ='ghost white').place(x=158,y=120)
 
 window.mainloop()
